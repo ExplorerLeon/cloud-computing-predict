81075c1be3cfffccbe2b1a9c355b1a043ca8be9e
ExplorerLeon <leon@explore-intelligence.net>
2023-02-10 14:03:06 +0200

    new api and lambda - test

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  --------
-                url: "https://5jd7alwcq1.execute-api.eu-west-1.amazonaws.com",
+                url: "https://gihizc9xn0.execute-api.eu-west-1.amazonaws.com",
                 // ------------------------------------------------------
                 type: "POST",
                 data: JSON.stringify(json),
